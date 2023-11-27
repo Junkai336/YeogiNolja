423d990a58f75d8b010106baa438d7bb3f0a22d9
Junkai336 <gjwnssla2@gmail.com>
2023-11-28 07:52:15 +0900

    11월 28일 07시50분 메인브렌치 커밋

--- member/src/main/java/com/example/member/controller/BoardController.java
+++ member/src/main/java/com/example/member/controller/BoardController.java
@@ -11,10 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -28,12 +28,15 @@ public class BoardController {
 
     @GetMapping(value = "/boardList")
     public String toBoard(Model model) {
+        List<Board> boardEntityList = boardRepository.findAll();
+        model.addAttribute("boardLists", boardEntityList);
+
         return "board/boardList";
     }
 
     @GetMapping(value = "/boardWrite")
     public String newBoard(Model model) {
-        BoardDto boardDto =new BoardDto();
+        BoardDto boardDto = new BoardDto();
         model.addAttribute("boardDto", boardDto);
         return "board/boardWrite";
     }
@@ -41,14 +44,52 @@ public class BoardController {
     @PostMapping(value = "/boardCreate")
     public String createBoard(BoardDto boardDto) {
 
-
     boardService.saveBoard(boardDto);
 
-
-//        Board saved = boardService.create(boardDto);
-
         //redirect : 브라우저가 해당 URL로 재요청
-//        return "redirect:/board/" + savedBoard.getId();
         return "redirect:/board/boardList";
     }
+
+    @GetMapping(value = "/board/{id}")
+    public String show (@PathVariable Long id, Model model) {
+        Board boardEntity = boardRepository.findById(id).orElse(null);
+        model.addAttribute("boards", boardEntity);
+        return "board/boardContents";
+    }
+
+    @GetMapping(value = "/board/{id}/boardEdit")
+    public String edit(@PathVariable Long id, Model model) {
+        Board boardEntity = boardRepository.findById(id).orElse(null);
+        model.addAttribute("board",boardEntity);
+        return "board/boardEdit";
+    }
+
+    @GetMapping(value = "/board/boardUpdate")
+    public String update(BoardDto boardDto) {
+        Board boardEntity = Board.builder()
+                .boardTitle(boardDto.getBoardTitle())
+                .content(boardDto.getContent())
+                .build();
+
+        Board target = boardRepository.findById(boardEntity.getId()).orElse(null);
+
+        if(target != null) {
+            boardRepository.save(boardEntity);
+
+        }
+            return "redirect:/board/boardList";
+    }
+
+    @GetMapping(value = "board/{id}/boardDelete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        Board target = boardRepository.findById(id).orElse(null);
+
+        if(target != null) {
+            boardRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제가 완료되었습니다.");
+        }
+
+        return "redirect:/board/boardList";
+    }
+
 }
