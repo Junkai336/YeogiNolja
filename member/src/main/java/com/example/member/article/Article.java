8ab7df1dae267a854fe04ea9d210a7872140db89
gijun96 <>
2023-12-04 12:58:17 +0900

    세이브
    기존 board 삭제-> article 패키지 생성

---
+++ member/src/main/java/com/example/member/article/Article.java
@@ -0,0 +1,49 @@
+package com.example.member.article;
+
+import com.example.member.constant.CategoryStatus;
+import com.example.member.entity.BaseEntity;
+import com.example.member.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
+import org.thymeleaf.util.StringUtils;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "article")
+public class Article extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "article_id")
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+    @Column
+    private CategoryStatus categoryStatus;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @Column
+    private Long views; // 조회 수
+
+    @Column
+    private Long recommendations; // 추천 수
+
+
+
+    public static Article createArticle(ArticleDto articleDto, Member member) {
+        Article article = new Article();
+        article.setTitle(articleDto.getTitle());
+        article.setContent(articleDto.getContent());
+        article.setMember(member);
+        article.setCategoryStatus(CategoryStatus.NOTICE);
+        return article;
+    }
+}
