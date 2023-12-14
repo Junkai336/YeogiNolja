9cb0050dc752dc7e6af2bf941e317b9e6bc7fb40
Junkai336 <gjwnssla2@gmail.com>
2023-12-14 17:51:52 +0900

    버릴예정
    444

--- member/src/main/java/com/example/member/controller/LodgingController.java
+++ member/src/main/java/com/example/member/controller/LodgingController.java
@@ -1,10 +1,16 @@
 package com.example.member.controller;
 
 import com.example.member.constant.ReservationStatus;
+import com.example.member.dto.ItemImgDto;
 import com.example.member.dto.LodgingDto;
 import com.example.member.dto.RoomDto;
+import com.example.member.entity.ItemImg;
 import com.example.member.entity.Lodging;
+import com.example.member.entity.Member;
 import com.example.member.entity.Room;
+import com.example.member.repository.ItemImgRepository;
+import com.example.member.repository.LodgingRepository;
+import com.example.member.reserv.ReservDto;
 import com.example.member.reserv.reservDate.ReservedDateService;
 import com.example.member.service.ItemImgService;
 import com.example.member.service.LodgingService;
@@ -21,9 +27,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +46,9 @@ public class LodgingController {
     private final ItemImgService itemImgService;
     private final UploadFileService uploadFileService;
     private final ReservedDateService reservedDateService;
+
+    private final LodgingRepository lodgingRepository;
+    private final ItemImgRepository itemImgRepository
 
     @GetMapping(value = "/registration")
     public String toRegistration(Model model) {
@@ -95,70 +106,73 @@ public class LodgingController {
     }
 
     // 일자 등록없이 기본 값(오늘, 내일) 일자로하여 예약이 가능한 방을 보여준다.
-//    @GetMapping(value = "/{lodging_id}")
-//    public String show(@PathVariable Long lodging_id, Model model){
-//
-//        uploadFileService.refreshUploadFileCheck();
-//
-//        Lodging lodging = lodgingRepository.findById(lodgingId).orElseThrow(EntityNotFoundException::new);
-//
-//        lodgingService.emptyRoomGrantedLodgingId(lodgingId, lodging);
-//
-//        LodgingDto lodgingDto = LodgingDto.toLodgingDto(lodging);
-//
-//        LodgingDto lodgingDtoContainImage =  lodgingService.imageLoad(lodgingDto, lodgingId);
-//
-//        for(ItemImgDto itemImgDto : lodgingDtoContainImage.getItemImgDtoList()) {
-//            System.out.println(itemImgDto);
-//        }
-//
-//        Member member = lodging.getMember();
-//
-//        lodgingDto.setMember(member);
-//
-//        List<ItemImg> lodgingItemImgList = itemImgService.findByLodgingId(lodgingId);
-//
-//
-//
-//        // ------------------------------------------------------------
-////        LocalDate defaultNow = LocalDate.now();
-////        LocalDate tomorrow = LocalDate.now().plusDays(1);
-//        List<RoomDto> roomDtoList = roomService.roomDtoList(lodgingId);
-//
-//        for (int i = 0; i < roomDtoList.size(); i++) {
-//            // 객실 DTO i번쨰 꺼내오기
-//            RoomDto roomDto = roomDtoList.get(i);
-//            // 꺼내온 숙소 DTO의 아이디를 조회하고 아이디에 맞는 이미지들을 리스트로 뽑아오기
-//            List<ItemImg> itemImgList = itemImgRepository.findByRoomId(roomDto.getId());
-//
-//            List<ItemImgDto> itemImgDtoList = new ArrayList<>();
-//
-//            for (ItemImg itemImg : itemImgList) {
-//                ItemImgDto itemImgDto = ItemImgDto.toItemImgDto(itemImg);
-//                itemImgDtoList.add(itemImgDto);
-//            }
-//
-//            // 숙소 DTO 대표 imgUrl을 저장하기 위한 과정
-//            for (int l = 0; l < itemImgDtoList.size(); l++) {
-//                ItemImgDto itemImgDto = itemImgDtoList.get(l);
-//
-//                if (itemImgDto.getRepimgYn().equals("Y") && itemImgDto.getRoom().getId().equals(roomDto.getId())) {
-//                    roomDto.setImgUrl(itemImgDto.getImgUrl());
-//                }
-//            }
-//
-//            // 숙소 DTO에 이미지 DTO 저장
-//            roomDto.setItemImgDtoList(itemImgDtoList);
-//            // 다시 숙소 DTO에 저장
-//            roomDtoList.set(i, roomDto);
-//
-//            // -----------------------------------------------------------
-//
-//            model.addAttribute("lodgingDto", lodgingDtoContainImage);
-//            model.addAttribute("checkForm", new ReservDto());
-//            model.addAttribute("roomDtoList", roomDtoList);
-//            model.addAttribute("lodgingItemImgList", lodgingItemImgList);
-//    }
+    @GetMapping(value = "/{lodging_id}")
+    public String show(@PathVariable Long lodging_id, Model model) {
+
+        uploadFileService.refreshUploadFileCheck();
+
+        Lodging lodging = lodgingRepository.findById(lodging_id).orElseThrow(EntityNotFoundException::new);
+
+        lodgingService.emptyRoomGrantedLodgingId(lodging_id, lodging);
+
+        LodgingDto lodgingDto = LodgingDto.toLodgingDto(lodging);
+
+        LodgingDto lodgingDtoContainImage = lodgingService.imageLoad(lodgingDto, lodging_id);
+
+        for (ItemImgDto itemImgDto : lodgingDtoContainImage.getItemImgDtoList()) {
+            System.out.println(itemImgDto);
+        }
+
+        Member member = lodging.getMember();
+
+        lodgingDto.setMember(member);
+
+        List<ItemImg> lodgingItemImgList = itemImgService.findByLodgingId(lodging_id);
+
+
+        // ------------------------------------------------------------
+//        LocalDate defaultNow = LocalDate.now();
+//        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        List<RoomDto> roomDtoList = roomService.roomDtoList(lodging_id);
+
+        for (int i = 0; i < roomDtoList.size(); i++) {
+            // 객실 DTO i번쨰 꺼내오기
+            RoomDto roomDto = roomDtoList.get(i);
+            // 꺼내온 숙소 DTO의 아이디를 조회하고 아이디에 맞는 이미지들을 리스트로 뽑아오기
+            List<ItemImg> itemImgList = itemImgRepository.findByRoomId(roomDto.getId());
+
+            List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+
+            for (ItemImg itemImg : itemImgList) {
+                ItemImgDto itemImgDto = ItemImgDto.toItemImgDto(itemImg);
+                itemImgDtoList.add(itemImgDto);
+            }
+
+            // 숙소 DTO 대표 imgUrl을 저장하기 위한 과정
+            for (int l = 0; l < itemImgDtoList.size(); l++) {
+                ItemImgDto itemImgDto = itemImgDtoList.get(l);
+
+                if (itemImgDto.getRepimgYn().equals("Y") && itemImgDto.getRoom().getId().equals(roomDto.getId())) {
+                    roomDto.setImgUrl(itemImgDto.getImgUrl());
+                }
+            }
+
+            // 숙소 DTO에 이미지 DTO 저장
+            roomDto.setItemImgDtoList(itemImgDtoList);
+            // 다시 숙소 DTO에 저장
+            roomDtoList.set(i, roomDto);
+
+            // -----------------------------------------------------------
+
+            model.addAttribute("lodgingDto", lodgingDtoContainImage);
+            model.addAttribute("checkForm", new ReservDto());
+            model.addAttribute("roomDtoList", roomDtoList);
+            model.addAttribute("lodgingItemImgList", lodgingItemImgList);
+        }
+
+        return "reserv/lodgingReservContennt";
+
+    }
 
     @GetMapping(value = "/{id}/lodgingForm")
     public String toUpdate(@PathVariable Long id, Model model, Principal principal) {
