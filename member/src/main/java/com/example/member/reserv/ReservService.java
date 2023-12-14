1c5f2a9f9d1d2629e5b33f5f9b1246a24fd74673
Junkai336 <gjwnssla2@gmail.com>
2023-12-14 16:20:32 +0900

    reserv 겹치는 부분 수정

--- member/src/main/java/com/example/member/reserv/ReservService.java
+++ member/src/main/java/com/example/member/reserv/ReservService.java
@@ -62,7 +62,6 @@ public class ReservService {
     }
 
     public ReservDto newReserv(Long roomId, Principal principal,String date) throws Exception{
-    public ReservDto newReservDto(Long roomId, Principal principal,String date) throws Exception{
         ReservDto reservDto = new ReservDto();
         String[] checkDate = date.split("~");
 
