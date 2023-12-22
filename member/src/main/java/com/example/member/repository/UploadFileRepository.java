f662a63b517feb7d544cadab781f16648118e768
Junkai336 <gjwnssla2@gmail.com>
2023-12-22 15:20:55 +0900

    1222

--- member/src/main/java/com/example/member/repository/UploadFileRepository.java
+++ member/src/main/java/com/example/member/repository/UploadFileRepository.java
@@ -12,6 +12,6 @@ public interface UploadFileRepository extends JpaRepository<UploadFile, Long>{
     @Query(nativeQuery = true, value = "select * from just_board.upload_file a where a.article_id= :article_id")
     List<UploadFile> findAllByArticleId(@Param("article_id") Long article_id);
 
-    @Query(nativeQuery = true, value = "select * from Lodging l where l.lodging_id= :lodging_id")
+    @Query(nativeQuery = true, value = "select * from just_board.upload_file l where l.lodging_id= :lodging_id")
     List<UploadFile> findAllByLodgingId(@Param("lodging_id") Long lodging_id);
 }
